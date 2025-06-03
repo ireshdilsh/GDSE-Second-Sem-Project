1c7f99b94ef328c1bdf90383bd985777ce60b097
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-03 22:45:18 +0530

    update CrossOrigin annotation to allow all origins in AdminController

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin()
+@CrossOrigin(origins = "*")
 public class AdminController {
 
 }
