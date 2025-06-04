7f8ab5a02775a32aa13e06f42c05f6c765ab1058
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-04 22:12:19 +0530

    fix: add missing @Column annotation for firstName and lastName in Admin entity

--- src/main/java/com/example/demo/entity/Admin.java
+++ src/main/java/com/example/demo/entity/Admin.java
@@ -20,9 +20,10 @@ public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
-    
     @Column(unique = true , nullable = false)
     private String email;
     @Column(nullable = false)
