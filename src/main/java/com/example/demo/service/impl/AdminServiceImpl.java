a7a9cca2948553a62d5ddfb0abfbdde9f2bfb851
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-04 21:09:57 +0530

    add AdminService interface and its implementation

---
+++ src/main/java/com/example/demo/service/impl/AdminServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.AdminService;
+
+@Service
+public class AdminServiceImpl implements AdminService{
+
+}
