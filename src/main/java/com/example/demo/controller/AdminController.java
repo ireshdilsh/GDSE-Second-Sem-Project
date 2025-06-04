c0975ff2507acca2232c82fb508d135c75d88757
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-04 21:51:09 +0530

    add addNewUser method to AdminService interface and implement in AdminController

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.AdminDto;
+import com.example.demo.entity.Admin;
 import com.example.demo.service.AdminService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -12,5 +17,11 @@ public class AdminController {
  
     @Autowired 
     AdminService adminService;
+
+    @PostMapping("/add/new/admin")
+    public Admin addNewUser(@RequestBody AdminDto dto) {
+        return adminService.addNewUser(dto);
+    }
+    
     
 }
--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -2,7 +2,12 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.AdminDto;
+import com.example.demo.entity.Admin;
+
 @Service
 public interface AdminService {
+
+    Admin addNewUser(AdminDto dto);
     
 }
