3210d58a501071367fb4356a38e32d58e6a13797
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-04 21:14:18 +0530

    add missing import for AdminRepository in AdminServiceImpl

--- src/main/java/com/example/demo/service/impl/AdminServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AdminServiceImpl.java
@@ -3,11 +3,13 @@ package com.example.demo.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.repository.AdminRepository;
 import com.example.demo.service.AdminService;
 
 @Service
 public class AdminServiceImpl implements AdminService{
 
     @Autowired
+    AdminRepository adminRepository;
 
 }
