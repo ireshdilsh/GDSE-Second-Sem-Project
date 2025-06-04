e8f44cbdaab526268f70c205a1dd7f8b3b2debd3
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-04 21:11:43 +0530

    add @Autowired annotation to AdminService in AdminController and AdminServiceImpl

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.service.AdminService;
+
 @RestController
 @CrossOrigin(origins = "*")
 public class AdminController {
  
+    @Autowired 
+    AdminService adminService;
+    
 }
