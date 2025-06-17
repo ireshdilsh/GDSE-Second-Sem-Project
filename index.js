a0d116e4eb4713a9db78269de60e93454e4f964d
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-17 11:40:31 +0530

    feat: update color and background styling for business, graph, and marketing sections on click events

--- index.js
+++ index.js
@@ -55,14 +55,14 @@ $('#buisness').on('click', function () {
     $('#marketing-section').css('display', 'none');
     $('#language-section').css('display', 'none');
     $('#web').css('color', '#373643');
-    $('#data').css('color', 'white');
-    $('#buisness').css('color', '#373643');
+    $('#data').css('color', '#373643');
+    $('#buisness').css('color', 'white');
     $('#graphc').css('color', '#373643');
     $('#marketing').css('color', '#373643');
     $('#language').css('color', '#373643');
     $('#web').css('background-color', 'transparent');
-    $('#data').css('background-color', '#373643');
-    $('#buisness').css('background-color', 'transparent');
+    $('#data').css('background-color', 'transparent');
+    $('#buisness').css('background-color', '#373643');
     $('#graphc').css('background-color', 'transparent');
     $('#marketing').css('background-color', 'transparent');
     $('#language').css('background-color', 'transparent');
@@ -76,15 +76,15 @@ $('#graphc').on('click', function () {
     $('#marketing-section').css('display', 'none');
     $('#language-section').css('display', 'none');
     $('#web').css('color', '#373643');
-    $('#data').css('color', 'white');
+    $('#data').css('color', '#373643');
     $('#buisness').css('color', '#373643');
-    $('#graphc').css('color', '#373643');
+    $('#graphc').css('color', 'white');
     $('#marketing').css('color', '#373643');
     $('#language').css('color', '#373643');
     $('#web').css('background-color', 'transparent');
-    $('#data').css('background-color', '#373643');
+    $('#data').css('background-color', 'transparent');
     $('#buisness').css('background-color', 'transparent');
-    $('#graphc').css('background-color', 'transparent');
+    $('#graphc').css('background-color', '#373643');
     $('#marketing').css('background-color', 'transparent');
     $('#language').css('background-color', 'transparent');
 })
@@ -97,16 +97,16 @@ $('#marketing').on('click', function () {
     $('#marketing-section').css('display', 'block');
     $('#language-section').css('display', 'none');
     $('#web').css('color', '#373643');
-    $('#data').css('color', 'white');
+    $('#data').css('color', '#373643');
     $('#buisness').css('color', '#373643');
     $('#graphc').css('color', '#373643');
-    $('#marketing').css('color', '#373643');
+    $('#marketing').css('color', 'white');
     $('#language').css('color', '#373643');
     $('#web').css('background-color', 'transparent');
-    $('#data').css('background-color', '#373643');
+    $('#data').css('background-color', 'transparent');
     $('#buisness').css('background-color', 'transparent');
     $('#graphc').css('background-color', 'transparent');
-    $('#marketing').css('background-color', 'transparent');
+    $('#marketing').css('background-color', '#373643');
     $('#language').css('background-color', 'transparent');
 })
 
