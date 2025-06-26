1dd41e8742ed431f0345fc459bec900ce5fe3a75
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-26 23:02:05 +0530

    feat: replace signup component with navbar component in app structure

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignupComponent } from './signup-component/signup-component';
+import { NavbarComponent } from './navbar-component/navbar-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SignupComponent],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
