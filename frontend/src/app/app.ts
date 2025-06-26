e9e6aae5078f65120dc103de54139ea31ac5ceec
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-26 22:53:56 +0530

    feat: add navbar and signup components with initial structure and tests

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignupComponent } from './signup-component/signup-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SignupComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
