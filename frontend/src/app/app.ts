559302876f73ad46d2cd8ee721356fecaaf6dc46
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-27 09:17:28 +0530

    feat: add landing component and integrate into app structure style: enhance navbar component styles for improved layout and design

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignupComponent } from './signup-component/signup-component';
 import { NavbarComponent } from './navbar-component/navbar-component';
+import { LandingComponent } from './landing-component/landing-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,LandingComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
