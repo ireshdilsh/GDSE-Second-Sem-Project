ad5dc63b60c142edb5b12bf1988b39e25fe79eca
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-28 10:28:18 +0530

    feat: integrate navbar, landing, and footer components into main component

--- frontend/src/app/main-component/main-component.ts
+++ frontend/src/app/main-component/main-component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar-component/navbar-component';
+import { LandingComponent } from '../landing-component/landing-component';
+import { FooterComponent } from '../footer-component/footer-component';
 
 @Component({
   selector: 'app-main-component',
-  imports: [],
+  imports: [NavbarComponent,LandingComponent,FooterComponent],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css'
 })
