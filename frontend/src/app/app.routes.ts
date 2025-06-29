de6e204532d2480e6aa5b800397bd767a0f7b1f9
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-29 14:06:28 +0530

    feat: restructure app by removing MainComponent and integrating DashboardComponent with routing; update NavbarComponent template

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { NavbarComponent } from './navbar-component/navbar-component';
+import { DashboardComponent } from './dashboard-component/dashboard-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path : '' , component : DashboardComponent},
+   {path : 'navbar', component : NavbarComponent}
+];
