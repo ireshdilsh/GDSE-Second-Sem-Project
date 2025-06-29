6a698f83728640fb685a752458063c2a45920eeb
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-29 17:13:56 +0530

    fix: remove unused imports and clean up landing component files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './navbar-component/navbar-component';
 import { LandingComponent } from './landing-component/landing-component';
+import { ChatbotComponent } from './chatbot-component/chatbot-component';
 
 export const routes: Routes = [
    {path : '' , component : LandingComponent},
-   {path : 'navbar', component : NavbarComponent}
+   {path : 'navbar', component : NavbarComponent},
+   {path : 'ask/ai/agent', component : ChatbotComponent }
 ];
