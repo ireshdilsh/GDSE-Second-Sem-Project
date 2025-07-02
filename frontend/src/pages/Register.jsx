ca95f880c80841092ec56f1eaac8142686417735
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-02 18:07:54 +0530

    feat: add structure and styles for Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import '../styles/register.css'
+import logo from '../assets/logo.png'
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <div>
+            <div className="register-form">
+                <img src={logo} alt="logo" />
+                <p id='title'>Join SkillSpark – Learn. Teach. Grow.</p>
+                <p id='description'>Create your free account to start learning new skills or sharing your own.
+                </p>
+
+                <div className="goto-register">
+                    <p></p>
+                   
+                </div>
+            </div>
+        </div>
+    )
 }
