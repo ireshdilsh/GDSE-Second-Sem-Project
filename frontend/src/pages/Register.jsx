a506a4a07317799663ae72003c1c03c573f8a479
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-03 20:33:59 +0530

    feat: implement email navigation in Register page for improved signup flow

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import '../styles/register.css'
 import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Register() {
+
+    const navigate = useNavigate()
+
+    const gotoEmailWithSinUp = () => {
+        navigate('/sign/up/with/email')
+    }
+
     return (
         <div>
             <div className="register-form">
@@ -12,7 +19,7 @@ export default function Register() {
                 <p id='description'>Create your free account to start learning new skills or sharing your own.
                 </p>
                 <button id='google'>Continue With Google</button>
-                <button id='email'>Continue With Email</button>
+                <button id='email' onClick={gotoEmailWithSinUp}>Continue With Email</button>
                 <div className="goto-login">
                     <p>if you have an account ?</p>
                     <Link to="/navigate/user/auth/login">signin here</Link>
