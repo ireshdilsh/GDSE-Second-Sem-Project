09e83eee858d8c5901d7d5b2a33f7677b7b29f36
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-05 23:08:33 +0530

    feat: update project title to 'URBAN', remove unused assets and pages, and add Google Fonts for improved typography

--- frontend/src/Controller.jsx
+++ frontend/src/Controller.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import Landing from './pages/Landing'
+
 import { BrowserRouter, Route , Routes } from 'react-router-dom'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import EmailSignin from './pages/EmailSignin'
-import EmailSignup from './pages/EmailSignup'
+import Landing from './pages/Landing'
 
 export default function Controller() {
   return (
@@ -12,10 +9,6 @@ export default function Controller() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Landing/>}></Route>
-          <Route path='/navigate/user/create/account' element = {<Register/>}></Route>
-          <Route path='/navigate/user/auth/login' element = {<Login/>}></Route>
-          <Route path='/sign/in/with/email' element = {<EmailSignin/>}></Route>
-          <Route path='/sign/up/with/email' element = {<EmailSignup/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
