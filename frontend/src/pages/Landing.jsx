ecd8c6f5b159504a92baa120043767ecafc05d60
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-06 20:58:21 +0530

    feat: remove unused Landing page components and styles for cleaner codebase

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,25 +1,7 @@
 import React from 'react'
-import logo from '../assets/logo.png'
-import '../styles/landing.css'
 
 export default function Landing() {
   return (
-    <div>
-
-        <div className="glass">
-          <div className="logo">
-            <img src={logo} alt="" />
-          </div>
-          <div className="links">
-            <a href="">Home</a>
-            <a href="">MarketPlace</a>
-            <a href="">Blogs & Tips</a>
-            <a href="">GreenAI</a>
-            <a href="">About Us</a>
-            <a href="">Contact Us</a>
-          </div>
-        </div>
-
-    </div>
+    <div>Landing</div>
   )
 }
