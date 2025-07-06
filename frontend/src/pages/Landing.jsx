d09cd1bc30e136a220dfc655f619894825762c49
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-06 20:29:38 +0530

    feat: update title to 'UrbanGreen', add logo and hero image, and enhance Landing page styling for improved user experience

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import '../styles/landing.css'
 
 export default function Landing() {
   return (
     <div>
-      <h1>This Is LangiG Page</h1>
+
+        <div className="glass">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="links">
+            <a href="">Home</a>
+            <a href="">MarketPlace</a>
+            <a href="">Blogs & Tips</a>
+            <a href="">GreenAI</a>
+            <a href="">About Us</a>
+            <a href="">Contact Us</a>
+          </div>
+        </div>
+
     </div>
   )
 }
