42a66d571f4a46c3358acb5550bf4826d627fe2d
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-07 23:28:52 +0530

    feat: enhance Landing page with navigation, logo, and improved layout for better user experience

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/landing.css'
 import bgVideo from '../assets/Green Aesthetic Forest Nature Video.mp4'
+import logo from '../assets/logo.png'
 
 export default function Landing() {
   return (
@@ -8,7 +9,34 @@ export default function Landing() {
 
       <video src={bgVideo} autoPlay loop muted>
       </video>
-    
+
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="links">
+          <a href="">Home</a>
+          <a href="">Marketplace</a>
+          <a href="">GreenAI</a>
+          <a href="">Blogs & Tips</a>
+          <a href="">About Us</a>
+          <a href="">Contact Us</a>
+        </div>
+      </nav>
+
+      <section id="home">
+        <div className="left-side">
+          <p id='title'>Grow Together, Swap Freshness.</p>
+          <p id='sub-title'>Join the Urban Green — your community
+            garden exchange.</p>
+          <p id='description'>Discover a vibrant network of local gardeners trading homegrown fruits, vegetables, herbs, and plants. Share your harvest, find what you need, and connect
+            with nature-loving neighbors — all in one simple, green circle.</p>
+          <button>Get Started</button>
+        </div>
+        <div className="right-side">
+          <h3>Create Account</h3>
+        </div>
+      </section>
     </div>
   )
 }
