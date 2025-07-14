c655d09c81b66f024a30612b27d031327301fdee
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-14 21:51:38 +0530

    update sing ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import logo from '../assets/footer-logo.png'
+import '../styles/signin.css'
+import { Link } from 'react-router-dom'
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <img src={logo} alt="" id='logo' />
+      <h3>Sign In</h3>
+      <div className="offer">
+        <p>New shoppers get 10% off their first order.</p>
+      </div>
+      <p><img id='protect-icn' src="https://img.icons8.com/?size=100&id=16231&format=png&color=000000" alt="" /> Your information is protected.</p>
+      <div className="email">
+        <label htmlFor="">Email Address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+      </div>
+      <div className="password">
+        <label htmlFor="">Password</label>
+        <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+      </div>
+      <button>Goto Dashboard</button>
+    </div>
   )
 }
