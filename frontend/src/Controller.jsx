9fb33782afe9ee5f5ce50621e46553adcfc03226
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-15 22:44:32 +0530

    add userdashboard path

--- frontend/src/Controller.jsx
+++ frontend/src/Controller.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter, Route , Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
+import UserDashboard from './pages/UserDashboard'
 
 export default function Controller() {
   return (
@@ -11,6 +12,7 @@ export default function Controller() {
         <Routes>
           <Route path='/' element={<Landing/>}></Route>
           <Route path='/goto/user/auth/signin' element = {<Login/>}></Route>
+          <Route path='/user/dashboard' element = {<UserDashboard/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
