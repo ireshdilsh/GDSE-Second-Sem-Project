8791cc34c34d6eab51b3ddf14594100c3fe4fc22
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-01 19:31:03 +0530

    add UserDto, UserRepository, and UserServiceImpl classes for user management

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
+}
