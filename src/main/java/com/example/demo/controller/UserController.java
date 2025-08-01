84f4194586b58a53d833d6158c4ef60b237bfa11
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-01 19:49:08 +0530

    implement user creation functionality in UserServiceImpl and UserController

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,35 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.service.UserService;
+import com.example.demo.utils.APIResponse;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/v1")
 public class UserController {
+
+    @Autowired
+    UserService service;
+
+    // private static final Logger logger = 
+
+    @PostMapping("/save/user")
+    public ResponseEntity<APIResponse> postMethod(@RequestBody UserDto dto) {
+        service.postMethod(dto);
+        return new ResponseEntity<>(new APIResponse(200, "success",dto), HttpStatus.CREATED);
+    }
     
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.UserDto;
+import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
@@ -15,5 +17,10 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     ModelMapper modelMapper;
+
+    @Override
+    public void postMethod(UserDto dto) {
+        userRepository.save(modelMapper.map(dto, User.class));
+    }
 
 }
