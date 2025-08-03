86472f94e16aa7df944a71f33bf6820f44f7f1c0
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-03 12:12:48 +0530

    refactor(CategoryServiceImpl): add logger and modelMapper initialization

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.example.demo.service.impl;
 
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.controller.CategoryController;
+import com.example.demo.repository.CategoryRepository;
 import com.example.demo.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
@@ -10,5 +16,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
-    
+    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
+    ModelMapper modelMapper;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 }
