882c638bcfe01195adc7b4413c1a2cf224973189
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-03 10:35:53 +0530

    add @Service annotation to ContactService and ContactServiceImpl; add @Repository annotation to ContactRepository

--- src/main/java/com/example/demo/service/impl/ContactServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ContactServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.demo.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.service.ContactService;
 
+@Service
 public class ContactServiceImpl implements ContactService{
 
 }
