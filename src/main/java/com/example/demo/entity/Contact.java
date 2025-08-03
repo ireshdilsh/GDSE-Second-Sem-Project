ae9cc3b59c35ffbcbd0e86f335f7ca54116743c3
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-03 10:34:06 +0530

    create contact repository and make auto generate id for contact enity class

--- src/main/java/com/example/demo/entity/Contact.java
+++ src/main/java/com/example/demo/entity/Contact.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String subject;
