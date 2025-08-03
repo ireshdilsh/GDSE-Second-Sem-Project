987da3a04e5fe33699bc26d94ffd41a82bc06688
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-03 10:30:55 +0530

    make email uniqe

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -17,8 +17,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
 }
