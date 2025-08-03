55b94aa34a52cca5f633b03591167368ae5a3ded
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-03 12:11:47 +0530

    create CategoryService interface and CategoryServiceImpl implementation

---
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.CategoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    
+}
