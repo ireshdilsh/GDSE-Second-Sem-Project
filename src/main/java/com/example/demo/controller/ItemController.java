4cd290105a168b4670b5e9a58674348208a7b339
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-04 15:29:47 +0530

    feat(ItemController): add initial controller setup for product API

---
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@CrossOrigin(origins = "*")
+public class ItemController {
+
+    // Create Post Method
+    
+}
