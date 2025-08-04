e32ab6ea012be22cb0aaa1d6be027a24995af26f
ireshdilsh <ireshdilshan46@gmail.com>
2025-08-04 17:40:19 +0530

    update

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import com.example.demo.utils.APIResponse;
 
@@ -26,15 +25,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new APIResponse(404, "Already Exists Category Name", null),HttpStatus.BAD_REQUEST); 
     }
 
-    @ExceptionHandler(ImageProcessingException.class)
-    public ResponseEntity<APIResponse> handleImageProcessingException(ImageProcessingException e) {
-        return new ResponseEntity<>(new APIResponse(400, "Image Processing Error: " + e.getMessage(), null), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<APIResponse> handleMaxUploadSizeExceeded(MaxUploadSizeExceededException e) {
-        return new ResponseEntity<>(new APIResponse(413, "File size exceeds maximum allowed size", null), HttpStatus.PAYLOAD_TOO_LARGE);
-    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<APIResponse> handleRuntimeException(RuntimeException e) {
