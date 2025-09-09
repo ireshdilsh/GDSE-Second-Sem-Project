30db05ee995a4415fdd31f9d90d4d40f94fac155
ireshdilsh <ireshdilshan46@gmail.com>
2025-09-09 16:19:18 +0530

    feat: implement About page with navigation and styling

--- frontend/src/Controller.jsx
+++ frontend/src/Controller.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import '../src/index.css'
 import Landing from './pages/Landing'
+import About from './pages/About'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function Controller() {
     return (
         <div>
-            <Landing/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Landing/>}></Route>
+                    <Route path='/about' element={<About/>}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
