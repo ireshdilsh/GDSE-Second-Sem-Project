64e9328c08a416af23c5f3721793c35c38860bdc
ireshdilsh <ireshdilshan46@gmail.com>
2025-09-10 16:10:39 +0530

    feat: update Write page with reading time estimation and improved layout; enhance styling for better user experience

--- frontend/src/pages/Write.jsx
+++ frontend/src/pages/Write.jsx
@@ -10,6 +10,18 @@ export default function Write() {
         setShowProfileMenu(!showProfileMenu);
     };
 
+    const [content, setContent] = useState('')
+
+    const btn = () =>{
+        const readingCount = estimateReadingTime(content)
+        alert("reading time is "+ readingCount);
+    }
+
+    const estimateReadingTime = (text) => {
+        const words = text.trim().split(/\s+/).filter(Boolean).length;
+        return Math.max(0, Math.ceil(words / 15));
+    }
+
     return (
         <div className='write-page'>
             <nav className="write-nav">
@@ -18,7 +30,7 @@ export default function Write() {
                     <p>Draft</p>
                 </div>
                 <div className="right-side">
-                    <button>Add to Draft</button>
+                    <button onClick={btn}>Add to Draft</button>
                     <img src="https://img.icons8.com/?size=100&id=83989&format=png&color=5D5D5D" alt="notification-img" />
                     <div className="profile" onClick={toggleProfileMenu}>
                         <img src="https://img.icons8.com/?size=100&id=7819&format=png&color=5D5D5D" alt="profile-img" /><span>Iresh Dilshan</span>
@@ -65,11 +77,19 @@ export default function Write() {
                     <h4>Draft Mode Active</h4>
                     <p>Your article is directly saved to drafts as you write. This gives you time to review and correct any grammatical errors or improve content before publishing. Once ready, you can publish directly from your drafts.</p>
                 </div>
-                <button className="close-info" onClick={(e) => {
-                    e.target.closest('.info-message').style.display = 'none';
-                }}>
-                    &times;
-                </button>
+            </div>
+            <section className="write-area">
+                <div className="title-area">
+                    <input type="text" placeholder='Title' />
+                </div>
+               
+                <div className="description-area">
+                    <textarea value={content} onChange={(e) => { setContent(e.target.value) }} name="" id="" placeholder='Tell your story...'></textarea>
+                </div>
+
+            </section>
+            <div className="reading-time-display">
+                <p>Estimated Reading Time: {estimateReadingTime(content)} min</p>
             </div>
         </div>
     )
