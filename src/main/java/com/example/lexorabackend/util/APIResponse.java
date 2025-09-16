aeac50c07f08d90683c9a10f6508852b7672bc14
ireshdilsh <ireshdilshan46@gmail.com>
2025-09-16 22:46:14 +0530

    enhance APIResponse structure and update application properties for database configuration

--- src/main/java/com/example/lexorabackend/util/APIResponse.java
+++ src/main/java/com/example/lexorabackend/util/APIResponse.java
@@ -1,5 +1,14 @@
 package com.example.lexorabackend.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class APIResponse {
+    private int status;
+    private String message;
+    private Object data;
 }
