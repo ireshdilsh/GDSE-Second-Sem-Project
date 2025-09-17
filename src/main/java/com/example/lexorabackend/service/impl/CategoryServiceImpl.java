8d4ab4715b888bbdf9ab2d58babd4d290e67dc2b
ireshdilsh <ireshdilshan46@gmail.com>
2025-09-17 09:37:23 +0530

    add category management functionality with service methods for adding, retrieving, updating, and deleting categories

--- src/main/java/com/example/lexorabackend/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/lexorabackend/service/impl/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.lexorabackend.service.impl;
 
+import com.example.lexorabackend.dto.CategoryDto;
+import com.example.lexorabackend.entity.Cateory;
 import com.example.lexorabackend.repository.CategoryRepository;
 import com.example.lexorabackend.service.CategoryService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +9,11 @@ import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,4 +25,50 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Override
+    public CategoryDto addNewCategory(CategoryDto categoryDto) {
+        logger.info("Adding new category");
+        return modelMapper.map(categoryRepository.save(modelMapper.map(categoryDto,Cateory.class)), CategoryDto.class);
+    }
+
+    @Override
+    public List<CategoryDto> getAllCategory() {
+        logger.info("Getting all categories");
+        List<Cateory>cateories =  categoryRepository.findAll();
+
+        if (cateories.isEmpty()) {
+            logger.info("No categories found");
+            throw new RuntimeException("No categories found");
+        }
+        return categoryRepository.findAll().stream().map(cateory -> modelMapper.map(cateory,CategoryDto.class)).toList();
+    }
+
+    @Override
+    public CategoryDto updateCategory(Long id, CategoryDto categoryDto) {
+        logger.info("Updating category");
+        Cateory cateory =  categoryRepository.findById(id).get();
+        if (cateory == null) {
+            logger.info("No category found");
+            throw new RuntimeException("No category found");
+        }
+        cateory.setCategoryName(categoryDto.getCategoryName());
+        Cateory updateCategory = categoryRepository.save(cateory);
+        logger.info("Category updated successfully");
+        return modelMapper.map(updateCategory,CategoryDto.class);
+    }
+
+    @Override
+    public CategoryDto deleteCategory(Long id) {
+        logger.info("Deleting category");
+        Cateory cateory =  categoryRepository.findById(id).get();
+        if (cateory == null) {
+            logger.info("No category found");
+            throw new RuntimeException("No category found");
+        } else {
+            categoryRepository.delete(cateory);
+            logger.info("Category deleted successfully");
+            return modelMapper.map(cateory,CategoryDto.class);
+        }
+    }
 }
